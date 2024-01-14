9d48700bdbb3619eedd1c2c97b9be1edb75eddde
mergik <gmerlix@yahoo.com>
2024-01-15 00:49:33 +0300

    0. Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" this module contains
+a method that determines if all the boxes can be opened """
+
+
+def canUnlockAll(boxes):
+    """ method that determines if all the boxes can be opened """
+    keys = set()
+    for i in range(len(boxes)):
+        if i not in keys and i != 0:
+            return False
+        for elem in boxes[i]:
+            if elem < len(boxes):
+                keys.update(boxes[elem])
+        keys.update(boxes[i])
+    return True
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,5 @@
+# 0x01. Lockboxes
+
+| File Name | Description     |
+| ------------ | ------------    |
+| `0-lockboxes.py` | Method that determines if all the boxes can be opened |
