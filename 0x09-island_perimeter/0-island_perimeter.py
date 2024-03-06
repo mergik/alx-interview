88d3bdb079db9d0b1e1c34ab559be7a7783f3012
mergik <gmerlix@yahoo.com>
2024-03-07 00:01:00 +0300

    Fixed pycodestyle issue

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """returns the perimeter of the island described in grid"""
 
+
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
     total = 0
